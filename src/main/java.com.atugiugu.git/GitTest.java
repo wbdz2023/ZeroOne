4a4f84d0b79e97b2e751419514c3f554ab400de5
wbdz2023 <140807250+wbdz2023@users.noreply.github.com>
2023-07-29 00:46:46 +0800

    Update GitTest.java

--- src/main/java.com.atugiugu.git/GitTest.java
+++ src/main/java.com.atugiugu.git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("我不要impart");
         System.out.println("我要开impart");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
